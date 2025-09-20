b47cd294078a7b352c2dec51034ac6b80fdd6c6b
Thiên Phú <phuvanideptrai@gmail.com>
2025-09-20 23:06:54 +0700

    Add files via upload

---
+++ LTDT_BACVAOBACRA_3124411232.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    ifstream fin("BacVaoBacRa.inp");
+    ofstream fout("BacVaoBacRa.out");
+    if (!fin) {
+        cerr << "Không mở được file vào.\n";
+        return 1;
+    }
+    int n;
+    fin >> n;
+    int indeg[1000] = { 0 };
+    int outdeg[1000] = { 0 };
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            int x;
+            fin >> x;
+            if (x) {
+                outdeg[i]++;
+                indeg[j]++;
+            }
+        }
+    }
+
+    fout << n << '\n';
+    for (int i = 0; i < n; ++i) {
+        fout << indeg[i] << ' ' << outdeg[i] << '\n';
+    }
+
+    // In ra màn hình để kiểm tra nhanh (không bắt buộc)
+    cout << "Da ghi ket qua ra BacVaoBacRa.out\n";
+    cout << n << '\n';
+    for (int i = 0; i < n; ++i) cout << indeg[i] << ' ' << outdeg[i] << '\n';
+
+    return 0;
+}
